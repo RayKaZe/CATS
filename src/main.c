ea894330445c5f899256b65be6054b9dbc3bef17
Leo Zhou <leothemagnificent@gmail.com>
2015-04-05 10:28:00 +0100

    Split the splash_window code from main.c into dedicated files.

--- src/main.c
+++ src/main.c
@@ -3,15 +3,11 @@
 
 #include "barcode.h"
 #include "database.h"
+#include "splash_window.h"
 
 Window* menu_window;
 Window* bar_code_window;
-Window* splash_window;
 MenuLayer *menu_layer;
-TextLayer *splash_logo;
-static GBitmap *splash_cat_bitmap;
-static BitmapLayer *splash_cat_bitmap_layer;
-InverterLayer *cat_inverter_layer;
 BitmapLayer *barcode;
 TextLayer *footer;
 GBitmap *bmp1;
@@ -118,16 +114,6 @@ void select_click_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *c
    }
 }
 
-void long_click_handler(ClickRecognizerRef recognizer, void *context)
-{
-  clear_persist();
-}
-
-void config_provider(Window *window)
-{
-  window_long_click_subscribe(BUTTON_ID_UP, 0, long_click_handler, NULL);
-}
-
 void menu_window_load(Window *window)
 {
     //Create it - 12 is approx height of the top bar
@@ -151,49 +137,6 @@ void menu_window_load(Window *window)
 void menu_window_unload(Window *window)
 {
  menu_layer_destroy(menu_layer);
-}
-
-void splash_timer_callback()
-{
-  window_stack_pop(true);
-}
-
-void splash_window_load(Window *window)
-{
-  GRect cat_bounds= GRect(22, 65, 99, 77);
-  
-  // Create time TextLayer
-  splash_logo = text_layer_create(GRect(0, 10, 144, 50));
-  text_layer_set_background_color(splash_logo, GColorClear);
-  text_layer_set_text_color(splash_logo, GColorClear);
-  text_layer_set_text(splash_logo, "CATS");
-
-  // Improve the layout to be more like a watchface
-  text_layer_set_font(splash_logo, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-  text_layer_set_text_alignment(splash_logo, GTextAlignmentCenter);
-
-  // Add it as a child layer to the Window's root layer
-  layer_add_child(window_get_root_layer(splash_window), text_layer_get_layer(splash_logo));
-  
-  // cat image
-  splash_cat_bitmap = gbitmap_create_with_resource(RESOURCE_ID_SPLASH_CAT);
-  
-  splash_cat_bitmap_layer = bitmap_layer_create(cat_bounds);
-  bitmap_layer_set_bitmap(splash_cat_bitmap_layer, splash_cat_bitmap);
-  layer_add_child(window_get_root_layer(splash_window), bitmap_layer_get_layer(splash_cat_bitmap_layer));
-  
-  cat_inverter_layer = inverter_layer_create(cat_bounds);
-  layer_add_child(window_get_root_layer(splash_window), inverter_layer_get_layer(cat_inverter_layer));
-  
-  window_set_click_config_provider(splash_window, (ClickConfigProvider) config_provider);
-  
-  app_timer_register(2000, splash_timer_callback, NULL);
-}
-
-void splash_window_unload(Window *window)
-{
-  window_destroy(window);
-  text_layer_destroy(splash_logo);
 }
 
 static void in_recv_handler(DictionaryIterator *iterator, void *context)
@@ -259,21 +202,15 @@ void init()
     */
     setup_entry_db();
     menu_window = window_create();
-    splash_window = window_create();
     WindowHandlers handlers = {
         .load = menu_window_load,
         .unload = menu_window_unload
     };
     window_set_window_handlers( menu_window, (WindowHandlers) handlers );
     window_stack_push(menu_window, true);
-  
-    handlers.load = splash_window_load;
-    handlers.unload = splash_window_unload;
-    window_set_window_handlers( splash_window, (WindowHandlers) handlers );
-    window_set_fullscreen(splash_window, true);
-    window_set_background_color(splash_window, GColorBlack);
-    window_stack_push(splash_window, false);
-  
+
+    splash_window_init();
+
     app_message_register_inbox_received((AppMessageInboxReceived) in_recv_handler);
     app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
 }
---
+++ src/splash_window.c
@@ -0,0 +1,76 @@
+#include <pebble.h>
+#include "database.h"
+
+Window* splash_window;
+TextLayer *splash_logo;
+static GBitmap *splash_cat_bitmap;
+static BitmapLayer *splash_cat_bitmap_layer;
+InverterLayer *cat_inverter_layer;
+
+void long_click_handler(ClickRecognizerRef recognizer, void *context)
+{
+  clear_persist();
+}
+
+void config_provider(Window *window)
+{
+  window_long_click_subscribe(BUTTON_ID_UP, 0, long_click_handler, NULL);
+}
+
+void splash_window_unload(Window *window)
+{
+  window_destroy(window);
+  text_layer_destroy(splash_logo);
+}
+
+void splash_timer_callback()
+{
+  window_stack_pop(true);
+}
+
+void splash_window_load(Window *window)
+{
+  GRect cat_bounds= GRect(22, 65, 99, 77);
+  
+  // Create time TextLayer
+  splash_logo = text_layer_create(GRect(0, 10, 144, 50));
+  text_layer_set_background_color(splash_logo, GColorClear);
+  text_layer_set_text_color(splash_logo, GColorClear);
+  text_layer_set_text(splash_logo, "CATS");
+
+  // Improve the layout to be more like a watchface
+  text_layer_set_font(splash_logo, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(splash_logo, GTextAlignmentCenter);
+
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_get_root_layer(splash_window), text_layer_get_layer(splash_logo));
+  
+  // cat image
+  splash_cat_bitmap = gbitmap_create_with_resource(RESOURCE_ID_SPLASH_CAT);
+  
+  splash_cat_bitmap_layer = bitmap_layer_create(cat_bounds);
+  bitmap_layer_set_bitmap(splash_cat_bitmap_layer, splash_cat_bitmap);
+  layer_add_child(window_get_root_layer(splash_window), bitmap_layer_get_layer(splash_cat_bitmap_layer));
+  
+  cat_inverter_layer = inverter_layer_create(cat_bounds);
+  layer_add_child(window_get_root_layer(splash_window), inverter_layer_get_layer(cat_inverter_layer));
+  
+  window_set_click_config_provider(splash_window, (ClickConfigProvider) config_provider);
+  
+  app_timer_register(2000, splash_timer_callback, NULL);
+}
+
+void splash_window_init()
+{
+  splash_window = window_create();
+  
+  WindowHandlers handlers = {
+    .load = splash_window_load,
+    .unload = splash_window_unload
+  };
+
+  window_set_window_handlers( splash_window, (WindowHandlers) handlers );
+  window_set_fullscreen(splash_window, true);
+  window_set_background_color(splash_window, GColorBlack);
+  window_stack_push(splash_window, false);
+}
