e6c8f8fc552f1657c100278fbf6b05358942579a
LIYOU ZHOU <leothemagnificent@gmail.com>
2015-12-08 23:53:30 +0000

    Modify return url to work with both emulator and phone

--- js/script.js
+++ js/script.js
@@ -53,6 +53,24 @@ function updateTable () {
   }
 }
 
+// Get query variables
+function getQueryParam(variable, defaultValue) {
+  // Find all URL parameters
+  var query = location.search.substring(1);
+  console.log("query", query);
+  var vars = query.split('&');
+  console.log("vars", vars)
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split('=');
+
+    // If the query variable parameter is found, decode it to use and return it for use
+    if (pair[0] === variable) {
+      return decodeURIComponent(pair[1]);
+    }
+  }
+  return defaultValue || false;
+}
+
 document.addEventListener("DOMContentLoaded", function(event) {
   $("#addcard_button").click( function () {
     if (checkInput() === 0) {
@@ -72,7 +90,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
   })
 
   $("#save_button").click( function() {
-    var location = 'pebblejs://close#'+ encodeURIComponent(JSON.stringify(cards));
+    // Set the return URL depending on the runtime environment
+    var return_to = getQueryParam('return_to', 'pebblejs://close#');
+    console.log(return_to)
+    var location = return_to + encodeURIComponent(JSON.stringify(cards));
     console.log(location);
     document.location = location;
   });
